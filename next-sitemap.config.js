c4fa00cfb47db930e5583eb50799d9e7f380fe2d
roboticsysinfo <roboticsysinfo@gmail.com>
2025-07-12 10:36:48 +0530

    next sitemap update

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://truerealstory.com',
-    generateRobotsTxt: true, // robots.txt bhi banega
-    sitemapSize: 5000,
-    changefreq: 'daily',
-    priority: 0.7,
-    exclude: ['/account/*'], // agar kuch routes exclude karne ho
-};
